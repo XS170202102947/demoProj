2defa379764a6068e61ab589ff348b37103596a9
XS170202102947 <122744532@qq.com>
2019-05-24 23:18:49 +0800

    第二次

--- demoProj/src/demoProj/jiaowu.java
+++ demoProj/src/demoProj/jiaowu.java
@@ -1,6 +1,6 @@
   package demoProj;
-			public class jiaowu {
-				public static void main(String[] args) {
-					System.out.println("good branch!");
-				}
+	public class jiaowu {
+		public static void main(String[] args) {
+		System.out.println("good branch!");
+		}
 }
